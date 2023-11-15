bdc49afb710140c4aaa639244840a366670a2ed9
aharon <ahronwm@gmail.com>
2023-11-15 15:17:27 +0200

    memory bord without features

--- main.js
+++ main.js
@@ -1,57 +1,79 @@
-let container = document.querySelector(".container");
-let card = document.querySelectorAll(".card");
+const tilesContainer = document.querySelector(".tiles");
+const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
+const colorsPicklist = [...colors, ...colors];
+const tileCount = colorsPicklist.length;
 
-let numOfCard = 16;
+// Game state
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
 
-function duobledArray() {
-  const duobl = [];
-  for (i = 1; i <= numOfCard; i++) {
-    duobl.push(i);
-    duobl.push(i);
-  }
-  return duobl;
+function buildTile(color) {
+	const element = document.createElement("div");
+
+	element.classList.add("tile");
+	element.setAttribute("data-color", color);
+	element.setAttribute("data-revealed", "false");
+
+	element.addEventListener("click", () => {
+		const revealed = element.getAttribute("data-revealed");
+
+		if (
+			awaitingEndOfMove
+			|| revealed === "true"
+			|| element == activeTile
+		) {
+			return;
+		}
+
+		// Reveal this color
+		element.style.backgroundColor = color;
+
+		if (!activeTile) {
+			activeTile = element;
+
+			return;
+		}
+
+		const colorToMatch = activeTile.getAttribute("data-color");
+
+		if (colorToMatch === color) {
+			element.setAttribute("data-revealed", "true");
+			activeTile.setAttribute("data-revealed", "true");
+
+			activeTile = null;
+			awaitingEndOfMove = false;
+			revealedCount += 2;
+
+			if (revealedCount === tileCount) {
+				alert("You win! Refresh to start again.");
+			}
+
+			return;
+		}
+
+		awaitingEndOfMove = true;
+
+		setTimeout(() => {
+			activeTile.style.backgroundColor = null;
+			element.style.backgroundColor = null;
+
+			awaitingEndOfMove = false;
+			activeTile = null;
+		}, 1000);
+	});
+
+	return element;
 }
-function randomize(values) {
-  let index = values.length;
-  let randomIndex;
 
-  // While there remain elements to shuffle.
-  while (index != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * index);
-    index--;
+// Build up tiles
+for (let i = 0; i < tileCount; i++) {
+	const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
+	const color = colorsPicklist[randomIndex];
+	const tile = buildTile(color);
 
-    // And swap it with the current element.
-    [values[index], values[randomIndex]] = [values[randomIndex], values[index]];
-  }
-
-  return values;
-  // return unsort
-  // return duobl
+	colorsPicklist.splice(randomIndex, 1);
+	tilesContainer.appendChild(tile);
 }
 
-let cards = duobledArray();
-console.log(cards);
-randomize(cards)
-
-console.log(cards);
-
-cards[
-
-
-// console.log(randomize(duobledArray()));
-
-for (let i = 0; i < numOfCard; i++) {
-  const div = document.createElement("div");
-  div.className = "card";
-  div.setAttribute("data-card", cards[i]);
-
-  div.addEventListener("click", function () {
-    div.className = "cardFlip";
-  });
-
-  container.appendChild(div);
-}
-// `card.addEventListener("click",function (){
-//     alert()
-// })`
+ 
